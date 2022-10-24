c0954bcbcb51425da3a477f356d4a6c783ad6736
Syed Hamed <79538432+ssyedhamed@users.noreply.github.com>
2022-10-25 04:09:41 +0530

    Delete SpringMVC directory

--- SpringMVC/src/main/java/com/springmvc/controller/Test.java
+++
@@ -1,9 +0,0 @@
-package com.springmvc.controller;
-
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-public class Test {
-//	DispatcherServlet
-//	InternalResourceViewResolver
-}
